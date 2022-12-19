83ab8e7a42065d35ff8d684a304f4aef02453be0
Andrea de Martinis <87342859+AndreadeMartinis@users.noreply.github.com>
2022-12-19 22:32:51 +0100

    Update controller.js

--- src/js/controller.js
+++ src/js/controller.js
@@ -10,10 +10,6 @@ import addRecipeView from './views/addRecipeView.js';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-if (module.hot) {
-  module.hot.accept();
-}
-
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
